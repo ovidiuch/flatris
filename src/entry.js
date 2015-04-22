20a0da6a888145b84d090aecf709a784fe306623
Ovidiu Cherecheș <skidding@gmail.com>
2015-04-22 19:47:25 +0300

    Expose serialize function alongside the root component #6

--- src/entry.js
+++ src/entry.js
@@ -1,8 +1,14 @@
 var React = require('react'),
+    ComponentTree = require('react-component-tree');
     FlatrisStatePersistor = require('./components/FlatrisStatePersistor.jsx');
 
 require('./style/reset.less');
 require('./style/flatris.less');
 
-module.exports = React.render(React.createElement(FlatrisStatePersistor, {}),
-                              document.getElementById('flatris'));
+exports.rootComponent = React.render(
+    React.createElement(FlatrisStatePersistor, {}),
+    document.getElementById('flatris'));
+
+exports.serialize = function() {
+  return ComponentTree.serialize(exports.rootComponent);
+};
