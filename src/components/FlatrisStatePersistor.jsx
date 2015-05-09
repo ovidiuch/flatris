3ba5fd7d1c71e46a090270e78119b13b0251469f
Ovidiu Chereches <skidding@gmail.com>
2015-05-10 02:21:11 +0300

    Remove snapshot from local storage #8

--- src/components/FlatrisStatePersistor.jsx
+++ src/components/FlatrisStatePersistor.jsx
@@ -2,7 +2,10 @@ var React = require('react'),
     _ = require('lodash'),
     $ = require('jquery'),
     ComponentTree = require('react-component-tree'),
+    FlatrisGame = require('./FlatrisGame.jsx'),
     FlatrisStatePreview = require('./FlatrisStatePreview.jsx');
+
+require('./FlatrisStatePersistor.less');
 
 class FlatrisStatePersistor extends ComponentTree.Component {
   /**
@@ -11,19 +14,33 @@ class FlatrisStatePersistor extends ComponentTree.Component {
   constructor() {
     super();
 
+    this.refreshSnapshot = this.refreshSnapshot.bind(this);
     this.onUnload = this.onUnload.bind(this);
 
+    this.state = {
+      snapshot: {}
+    };
+
     this.children = {
+      flatris: function() {
+        return {
+          component: FlatrisGame
+        };
+      },
       flatrisStatePreview: function() {
         return {
-          component: FlatrisStatePreview
+          component: FlatrisStatePreview,
+          snapshot: this.state.snapshot
         };
       }
     };
   }
 
   render() {
-    return this.loadChild('flatrisStatePreview');
+    return <div className="flatris-state-persistor">
+      {this.loadChild('flatris')}
+      {this.loadChild('flatrisStatePreview')}
+    </div>
   }
 
   componentDidMount() {
@@ -33,17 +50,24 @@ class FlatrisStatePersistor extends ComponentTree.Component {
     // a blank Flatris instance will be rendered
     var prevState = localStorage.getItem('flatrisState1');
     if (prevState) {
-      ComponentTree.injectState(this.refs.flatrisStatePreview,
-                                JSON.parse(prevState));
+      ComponentTree.injectState(this.refs.flatris, JSON.parse(prevState));
     }
+
+    this._refreshInterval = setInterval(this.refreshSnapshot, 200);
   }
 
   componentWillUnmount() {
     $(window).off('unload', this.onUnload);
+
+    clearInterval(this._refreshInterval);
+  }
+
+  refreshSnapshot() {
+    this.setState({snapshot: ComponentTree.serialize(this.refs.flatris)});
   }
 
   onUnload() {
-    var snapshot = ComponentTree.serialize(this.refs.flatrisStatePreview);
+    var snapshot = ComponentTree.serialize(this.refs.flatris);
     localStorage.setItem('flatrisState1', JSON.stringify(snapshot.state));
   }
 }
--- src/components/FlatrisStatePreview.jsx
+++ src/components/FlatrisStatePreview.jsx
@@ -1,60 +1,18 @@
-var React = require('react'),
-    ComponentTree = require('react-component-tree'),
-    FlatrisGame = require('./FlatrisGame.jsx');
+var React = require('react');
 
 require('./FlatrisStatePreview.less');
 
-class FlatrisStatePreview extends ComponentTree.Component {
+class FlatrisStatePreview extends React.Component {
   /**
-   * Render a Flatris instance next to its prettified, serialized state.
+   * Render the prettified, serialized state of a Flatris instance.
    */
-  constructor() {
-    super();
-
-    this.refreshSnapshot = this.refreshSnapshot.bind(this);
-
-    this.state = {
-      snapshot: '{}'
-    };
-
-    this.children = {
-      flatris: function() {
-        return {
-          component: FlatrisGame
-        };
-      }
-    };
-  }
-
   render() {
-    return <div className="flatris-state-preview">
-      {this.loadChild('flatris')}
-      <pre className="state-preview">{this.state.snapshot}</pre>
-    </div>;
+    return <pre className="flatris-state-preview">
+      {this._prettifyState(this.props.snapshot)}
+    </pre>;
   }
 
-  componentDidMount() {
-    this.refreshSnapshot();
-    this._intervalId = setInterval(this.refreshSnapshot, 200);
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    // No need to render for an identical snapshot
-    return nextState.snapshot != this.state.snapshot;
-  }
-
-  componentWillUnmount() {
-    clearInterval(this._intervalId);
-  }
-
-  refreshSnapshot() {
-    this.setState({
-      snapshot: this._serializeState(
-          ComponentTree.serialize(this.refs.flatris))
-    });
-  }
-
-  _serializeState(snapshot) {
+  _prettifyState(snapshot) {
     /**
      * This ugly method styles the indenting of the stringified state JSON.
      */
