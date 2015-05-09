07455677469ce5d4d70ad93d4312323be9656ef0
Ovidiu Chereches <skidding@gmail.com>
2015-05-09 23:26:46 +0300

    Update fixtures to new state #8

--- fixtures/Well/paused.js
+++ fixtures/Well/paused.js
@@ -4,6 +4,14 @@ module.exports = {
   rows: constants.WELL_ROWS,
   cols: constants.WELL_COLS,
   state: {
+    activeTetrimino: "T",
+    activeTetriminoGrid: constants.SHAPES.T,
+    activeTetriminoPosition: {
+      x: 2,
+      y: 13.508750000000028
+    },
+    animationLoopRunning: false,
+    gridBlockCount: 1199,
     grid: [
       [null,null,null,null,null,null,null,null,null,null],
       [null,null,null,null,null,null,null,null,null,null],
@@ -25,14 +33,6 @@ module.exports = {
       [null,null,null,null,null,null,null,null,null,null],
       [null,"1196#b04497",null,null,null,null,null,null,null,null],
       ["1197#b04497","1198#b04497","1199#b04497",null,null,null,null,null,null,null]
-    ],
-    gridBlockCount: 1199,
-    activeTetrimino: "T",
-    activeTetriminoGrid: constants.SHAPES.T,
-    activeTetriminoPosition: {
-      x: 2,
-      y: 13.508750000000028
-    },
-    animationLoopRunning: false
+    ]
   }
 };
--- fixtures/Well/with-accelerated-tetrimino-falling.js
+++ fixtures/Well/with-accelerated-tetrimino-falling.js
@@ -4,6 +4,20 @@ module.exports = {
   rows: constants.WELL_ROWS,
   cols: constants.WELL_COLS,
   state: {
+    activeTetrimino: "I",
+    activeTetriminoGrid: [
+      [0,0,1,0],
+      [0,0,1,0],
+      [0,0,1,0],
+      [0,0,1,0]
+    ],
+    activeTetriminoPosition: {
+      x: 1,
+      y: 2.3896774193548396
+    },
+    animationLoopRunning: true,
+    dropAcceleration: true,
+    gridBlockCount: 1430,
     grid: [
       [null,null,null,null,null,null,null,null,null,null],
       [null,null,null,null,null,null,null,null,null,null],
@@ -25,20 +39,6 @@ module.exports = {
       ["1420#e84138","1421#e84138","1413#3cc7d6",null,"1388#3cc7d6","1396#ed652f","1397#ed652f","1398#ed652f","1371#3cc7d6","1406#95c43d"],
       ["1422#e84138","1391#b04497","1414#3cc7d6",null,"1389#3cc7d6","1383#fbb414","1384#fbb414","1375#3993d0","1372#3cc7d6","1363#3cc7d6"],
       ["1392#b04497","1393#b04497","1379#3993d0",null,"1390#3cc7d6","1385#fbb414","1386#fbb414","1376#3993d0","1373#3cc7d6","1364#3cc7d6"]
-    ],
-    gridBlockCount: 1430,
-    activeTetrimino: "I",
-    activeTetriminoGrid: [
-      [0,0,1,0],
-      [0,0,1,0],
-      [0,0,1,0],
-      [0,0,1,0]
-    ],
-    activeTetriminoPosition: {
-      x: 1,
-      y: 2.3896774193548396
-    },
-    animationLoopRunning: true,
-    dropAcceleration: true
+    ]
   }
 };
