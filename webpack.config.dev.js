0e0fda808f0b3d4d7a6c283da2d338f7b2bf6dd8
Ovidiu Chereches <skidding@gmail.com>
2015-11-11 00:34:46 +0200

    Move babelrc in webpack config #9

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -21,8 +21,23 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
+      loader: 'babel',
+      include: path.join(__dirname, 'src'),
+      query: {
+        plugins: ["react-transform"],
+        extra: {
+          "react-transform": {
+            transforms: [{
+              transform: "react-transform-hmr",
+              imports: ["react"],
+              locals: ["module"]
+            }, {
+              transform: "react-transform-catch-errors",
+              imports: ["react", "redbox-react"]
+            }]
+          }
+        }
+      }
     }, {
       test: /\.less$/,
       loader: 'style-loader!css-loader!less-loader',
