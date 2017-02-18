ca0424152c2c8f69f5506261cd65c2ebb5f9382e
Ovidiu Cherecheș <skidding@gmail.com>
2017-02-18 17:14:31 +0200

    Add web manifesto

---
+++ src/components/Logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tetromino from './Tetromino';
+import { SHAPES } from '../constants/tetromino';
+
+import './Logo.css';
+
+const tetromino = 'Z';
+
+export default () => (
+  <div className="logo">
+    <Tetromino
+      key={tetromino}
+      color="#34495f"
+      grid={SHAPES[tetromino]}
+    />
+  </div>
+);
