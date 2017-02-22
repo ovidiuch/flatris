71b75b4142cb8b31a52885c8d6517e50b628bc56
Ovidiu Cherecheș <skidding@gmail.com>
2017-02-22 11:53:16 +0200

    Fix snapshots

--- src/components/InfoPanel.jsx
+++ src/components/InfoPanel.jsx
@@ -18,7 +18,8 @@ const InfoPanel = ({ styles }) => (
       Use the arrow keys or buttons below to play.
     </p>
     <p>
-      The game state is preserved between visits, so you can safely kill this tab when your employer is approaching and resume afterwards–including{' '}
+      The game state is preserved between visits, so you can safely kill this tab when your employer is approaching and resume afterwards–including
+      {' '}
       <a href="http://caniuse.com/#feat=serviceworkers">offline</a>
       !
     </p>
