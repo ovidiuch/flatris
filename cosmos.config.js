38dafba7f529b21c2c004768dae9ca7db0101e27
Ovidiu Cherecheș <skidding@gmail.com>
2017-03-10 17:00:36 +0100

    Use unresolved path to web pack config

--- cosmos.config.js
+++ cosmos.config.js
@@ -2,5 +2,5 @@ export default {
   componentPaths: ['src/components'],
   proxies: ['src/redux-proxy'],
   containerQuerySelector: '#root',
-  webpackConfigPath: './node_modules/react-scripts/config/webpack.config.dev.js'
+  webpackConfigPath: 'react-scripts/config/webpack.config.dev'
 };
