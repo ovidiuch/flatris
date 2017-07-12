6a451ae7313c389af982bc3d88666d70670ab51c
Ovidiu Cherecheș <skidding@gmail.com>
2017-07-12 14:19:49 +0200

    Remove .babelrc (#24)

--- cosmos.config.js
+++ cosmos.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   componentPaths: ['src/components'],
   proxies: ['react-cosmos-normalize-props-proxy', 'src/redux-proxy'],
   containerQuerySelector: '#root',
