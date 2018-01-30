d52b427d235bf319ad64ca854138dac28ca46252
Ovidiu Cherecheș <skidding@gmail.com>
2018-01-30 19:51:27 +0200

    Fix layout styles
    They needed to be set outside <Head>

--- components/Layout.js
+++ components/Layout.js
@@ -17,17 +17,17 @@ export default function({ children, title = 'Flatris' }: Props) {
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <style>{`
+      </Head>
+      <style jsx global>{`
         html,
         body {
           margin: 0;
           padding: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Ubuntu, 'Helvetica Neue',
-            sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, Ubuntu,
+            'Helvetica Neue', sans-serif;
           font-size: 16px;
         }
       `}</style>
-      </Head>
       {children}
     </div>
   );
