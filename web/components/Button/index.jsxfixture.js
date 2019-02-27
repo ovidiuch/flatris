779d019f49d005278a78d9bda8a4abe16253af66
Ovidiu Cherecheș <skidding@gmail.com>
2019-02-27 22:17:13 +0200

    Update index.jsxfixture.js

--- web/components/Button/index.jsxfixture.js
+++ web/components/Button/index.jsxfixture.js
@@ -5,31 +5,32 @@ import Button from '.';
 
 export default {
   primary: (
-    <Button
-      children="Press or be pressed"
-      onClick={() => console.log('Yay! Button clicked-ed!')}
-    />
+    <Button onClick={() => console.log('Yay! Button clicked-ed!')}>
+      Press or be pressed
+    </Button>
   ),
 
-  'primary disabled': <Button children="No chance amigo" disabled />,
+  'primary disabled': <Button disabled>No chance amigo</Button>,
 
   secondary: (
     <Button
-      children="Press me will you"
       bgColor="#fff"
       color="#34495f"
       colorDisabled="rgba(52, 73, 95, 0.6)"
       onClick={() => console.log('Yay! Button clicked-ed!')}
-    />
+    >
+      Press me will you
+    </Button>
   ),
 
   'secondary disabled': (
     <Button
-      children="Press me will you"
       disabled
       bgColor="#fff"
       color="#34495f"
       colorDisabled="rgba(52, 73, 95, 0.6)"
-    />
+    >
+      Press me will you
+    </Button>
   )
 };
