6d6e800a8945009e7013dd20c80f2cb211bf5e99
Ovidiu Cherecheș <skidding@gmail.com>
2019-03-10 13:24:22 +0200

    Upgrade to ReactRedux6

--- web/mocks/ReduxProviderMock.js
+++ web/mocks/ReduxProviderMock.js
@@ -1,91 +1,64 @@
 // @flow
 
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { isEqual } from 'lodash';
+import React, { useState, useContext, useEffect } from 'react';
+import { ReactReduxContext } from 'react-redux';
 import { FixtureContext } from 'react-cosmos-fixture';
 import { createStore } from '../store';
 
 import type { Node } from 'react';
-import type { FixtureContextValue } from 'react-cosmos-fixture';
 import type { State } from 'shared/types/state';
-import type { FlatrisReduxStore } from '../store';
 
 type Props = {
   children: Node,
   state: $Shape<State>
 };
 
-type LocalState = {
-  renderKey: number,
-  reduxState: void | State
-};
+// TODO: Differentiate between Redux provider in primary vs secondary renderers
+export function ReduxProviderMock({ children, state: mockedState }: Props) {
+  const { fixtureState, setFixtureState } = useContext(FixtureContext);
+  const initialState = (fixtureState && fixtureState.redux) || mockedState;
+  const [contextValue, setContextValue] = useCreateContextValue(initialState);
+  useStoreSubscribe(contextValue.store, setContextValue);
+  useUpdateReduxFixtureState(contextValue.store, setFixtureState);
 
-// TODO: Upgrade to react-redux@6
-export class ReduxProviderMock extends Component<Props, LocalState> {
-  static cosmosCapture = false;
-  static contextType = FixtureContext;
+  return (
+    <ReactReduxContext.Provider value={contextValue}>
+      {children}
+    </ReactReduxContext.Provider>
+  );
+}
 
-  // https://github.com/facebook/flow/issues/7166
-  context: FixtureContextValue;
+ReduxProviderMock.cosmosCapture = false;
 
-  store: FlatrisReduxStore = createStore(this.props.state);
-  storeUnsubscribe: void | (() => void);
+function useCreateContextValue(initialState) {
+  return useState(() => {
+    const store = createStore(initialState);
+    return {
+      storeState: store.getState(),
+      store
+    };
+  });
+}
 
-  state = {
-    renderKey: 0,
-    reduxState: undefined
-  };
-
-  componentDidMount() {
-    this.storeUnsubscribe = this.store.subscribe(this.updateFixtureState);
-  }
-
-  componentWillUnmount() {
-    if (this.storeUnsubscribe) {
-      this.storeUnsubscribe();
-    }
-  }
-
-  componentDidUpdate(prevProps: Props, prevState: LocalState) {
-    const localReduxStateChanged = !isEqual(
-      this.state.reduxState,
-      prevState.reduxState
-    );
-    if (localReduxStateChanged) {
-      return;
-    }
-
-    const { fixtureState } = this.context;
-    if (
-      fixtureState &&
-      fixtureState.reduxState &&
-      !isEqual(fixtureState.reduxState, this.store.getState())
-    ) {
-      this.store = createStore(fixtureState.reduxState);
-      this.setState({
-        renderKey: this.state.renderKey + 1
-      });
-    }
-  }
-
-  render() {
-    return (
-      <Provider key={this.state.renderKey} store={this.store}>
-        {this.props.children}
-      </Provider>
-    );
-  }
-
-  updateFixtureState = () => {
-    const reduxState = this.store.getState();
-    this.setState({ reduxState }, () => {
-      this.context.setFixtureState(fixtureState => {
-        return {
-          ...fixtureState,
-          reduxState
-        };
+function useStoreSubscribe(store, setContextValue) {
+  useEffect(() => {
+    return store.subscribe(() => {
+      setContextValue({
+        storeState: store.getState(),
+        store
       });
     });
-  };
+  }, []);
+}
+
+function useUpdateReduxFixtureState(store, setFixtureState) {
+  useEffect(
+    () => {
+      setFixtureState(fixtureState => ({
+        ...fixtureState,
+        redux: store.getState()
+      }));
+    },
+    [store.getState()]
+  );
 }
