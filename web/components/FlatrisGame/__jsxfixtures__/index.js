26c5e2670788aaa70b590f14d43dd39fe35cb023
Ovidiu Cherecheș <skidding@gmail.com>
2019-03-19 11:25:06 +0200

    Use Viewport decorator

--- web/components/FlatrisGame/__jsxfixtures__/index.js
+++ web/components/FlatrisGame/__jsxfixtures__/index.js
@@ -1,6 +1,25 @@
 // @flow
 
 import React from 'react';
+import { Viewport } from 'react-cosmos-fixture';
 import FlatrisGame from '../../FlatrisGame';
 
-export default <FlatrisGame />;
+export default {
+  'iPhone 5': (
+    <Viewport width={320} height={568}>
+      <FlatrisGame />
+    </Viewport>
+  ),
+
+  'iPhone 6 Plus': (
+    <Viewport width={414} height={736}>
+      <FlatrisGame />
+    </Viewport>
+  ),
+
+  Medium: (
+    <Viewport width={1024} height={768}>
+      <FlatrisGame />
+    </Viewport>
+  )
+};
